740436beb15b77b61e5b87ac6b45162c3d62c7ee
DanLeininger <Daniel.J.Leininger@gmail.com>
2020-06-08 16:25:13 -0400

    debug

--- src/helpers/compare-passwords.js
+++ src/helpers/compare-passwords.js
@@ -1,12 +1,15 @@
-
+const makeDebug = require('debug');
+const debug = makeDebug('authLocalMgnt:comparePwds');
 const bcrypt = require('bcryptjs');
 
 module.exports = comparePasswords;
 
 function comparePasswords (oldPassword, password, getError) {
   return new Promise((resolve, reject) => {
-    bcrypt.compare(oldPassword, password, (err, data1) =>
-      (err || !data1) ? reject(getError() || err) : resolve()
-    );
+    bcrypt.compare(oldPassword, password, (err, data1) => {
+      
+      debug('resetPassword', 'compare', err, data1);
+      return (err || !data1) ? reject(getError() || err) : resolve()
+    });
   });
 }

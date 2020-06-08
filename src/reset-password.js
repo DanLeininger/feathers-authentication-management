9e663cf3657fe376871c5679d9ab266f2f936bf8
DanLeininger <Daniel.J.Leininger@gmail.com>
2020-06-08 16:01:51 -0400

    womp

--- src/reset-password.js
+++ src/reset-password.js
@@ -61,7 +61,7 @@ async function resetPassword (options, query, tokens, password, field) {
 
   Object.keys(tokens).forEach(key => {
     debug('resetPassword', 'key', key);
-    debug('resetPassword', 'comparePasswords', tokens[key], user1[key);
+    debug('resetPassword', 'comparePasswords', tokens[key], user1[key]);
     promises.push(
       comparePasswords(
         tokens[key],
